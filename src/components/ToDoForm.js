81527a786f4d57b44c341175738fa4a2f2a2214d
supersexypanda <elena.est.ici@gmail.com>
2022-06-22 17:28:39 -0400

    finish the project

---
+++ src/components/ToDoForm.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect, useRef} from 'react'
+
+const ToDoForm = (props) => {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const todo = {
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        }
+
+        props.addTodo(todo);
+
+        setInput('');
+
+    };
+
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+        <input 
+            type="text"
+            placeholder={props.edit ? "Edit ToDo" : "Add ToDo"}
+            value={input} 
+            name="text" 
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+        />
+        <button className="todo-button">{props.edit ? "Update" : 'Add ToDo'}</button>
+    </form>
+  );
+  
+}
+
+export default ToDoForm;
